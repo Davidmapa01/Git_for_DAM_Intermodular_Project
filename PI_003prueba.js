54be1b9aa3f951ad7ffdc5d87ce5306c40a56268
vi-fe-ba <vifeba@alumnatflorida.es>
2025-12-05 19:26:00 +0100

    Ubicacion y fabricacion en .js

--- PI_003prueba.js
+++ PI_003prueba.js
@@ -1,20 +1,20 @@
 class Deposito {
-    constructor(liquido, litros) {
-        this.liquido = liquido;
-        this.litros = litros;
-    };
+  constructor(liquido, litros, ubicacion, fabricacion) {
+    this.liquido = liquido;
+    this.litros = litros;
+    this.ubicacion = ubicacion;
+    this.fabricacion = fabricacion;
+  }
 
-    // Como el ejecicio anterior pero sin mensaje por consola
-    Vaciar(cantidad) {
-        this.litros -= cantidad;
-    };
-};
+  // Como el ejecicio anterior pero sin mensaje por consola
+  Vaciar(cantidad) {
+    this.litros -= cantidad;
+  }
+}
 
+let depositoMunicipal = new Deposito("Agua", 100000, "Toledo", 2007);
 
-
-let depositoMunicipal = new Deposito("agua", 100000);
-
-// Se vacian 5000 litros 
-depositoMunicipal.Vaciar(5000)
+// Se vacian 5000 litros
+depositoMunicipal.Vaciar(5000);
 
 console.log(depositoMunicipal);
